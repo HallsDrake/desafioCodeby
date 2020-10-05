d3e5eb8d48a3e754bc6d9b35b1aa6d1d8143f93a
Gabriel Cestari <gabrielcolares.ti@gmail.com>
2020-10-05 04:37:38 -0300

    Entregando o projeto

---
+++ productStructure.js
@@ -0,0 +1,41 @@
+const products = [
+  "preto-PP",
+  "preto-M",
+  "preto-G",
+  "preto-GG",
+  "preto-GG",
+  "branco-PP",
+  "branco-G",
+  "vermelho-M",
+  "azul-XG",
+  "azul-XG",
+  "azul-XG",
+  "azul-P"
+]
+
+module.exports = () => {
+
+  const arr = [
+    preto = [
+      PP = 1,
+      M = 1,
+      G = 1,
+      GG = 2
+    ],
+    branco = [
+      PP = 1,
+      G = 1
+    ],
+    vermelho = [
+      M = 1
+    ],
+    azul = [
+      P = 1,
+      XG = 3
+    ]
+  ]
+
+  const array = Object.assign({}, arr); //Corverter array em OBJETO
+  console.log(array)
+
+}
